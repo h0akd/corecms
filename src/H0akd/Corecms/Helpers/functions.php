6100802159f86371b6ceaeb48f8285192e132508
Mai Ngọc Hoá <h0a.kd93@gmail.com>
2014-01-13 16:06:59 +0700

    Move add_routes_resources to funcstions utility

--- src/H0akd/Corecms/Helpers/functions.php
+++ src/H0akd/Corecms/Helpers/functions.php
@@ -16,3 +16,17 @@ function root_uri($path = "") {
         return "" . $path;
     }
 }
+
+/**
+ * 
+ * @param type $resource
+ * @param type $controller
+ */
+function add_routes_resources($resource, $controller) {
+    Route::get(root_uri() . '/' . $resource, array('as' => 'admin.' . $resource . '.index', 'uses' => $controller . "@index"));
+    Route::get(root_uri() . '/' . $resource . '/create', array('as' => 'admin.' . $resource . '.create', 'uses' => $controller . "@create"));
+    Route::post(root_uri() . '/' . $resource . '/store', array('as' => 'admin.' . $resource . '.store', 'uses' => $controller . "@store"));
+    Route::get(root_uri() . '/' . $resource . '/{id}/' . 'edit', array('as' => 'admin.' . $resource . '.edit', 'uses' => $controller . "@edit"));
+    Route::put(root_uri() . '/' . $resource . '/{id}/' . 'update', array('as' => 'admin.' . $resource . '.update', 'uses' => $controller . "@update"));
+    Route::put(root_uri() . '/' . $resource . '/{id}/' . 'destroy', array('as' => 'admin.' . $resource . '.destroy', 'uses' => $controller . "@destroy"));
+}
